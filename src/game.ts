94263549ef775dfb418f7c2c8d230936ea2e39e7
Sean <s.cunningham777@gmail.com>
2020-02-10 20:19:37 -0600

    4 spaces

--- src/game.ts
+++ src/game.ts
@@ -12,42 +12,42 @@ import { WORLD_WIDTH, WORLD_HEIGHT } from './constants';
 import { Plugins } from '@capacitor/core';
 
 const config: GameConfig = {
-  width: WORLD_WIDTH,
-  height: WORLD_HEIGHT,
-  type: Phaser.AUTO,
-  parent: 'game',
-  backgroundColor: '#93e7ff',
-  pixelArt: false,
-  zoom: 1,
-  physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 300 }
+    width: WORLD_WIDTH,
+    height: WORLD_HEIGHT,
+    type: Phaser.AUTO,
+    parent: 'game',
+    backgroundColor: '#93e7ff',
+    pixelArt: false,
+    zoom: 1,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 }
+        }
     }
-  }
 };
 
 const { StatusBar, SplashScreen } = Plugins;
 
 export class Game extends Phaser.Game {
 
-  constructor(GameConfig: config) {
+    constructor(GameConfig: config) {
 
-    super(config);
-    
-    this.scene.add('Boot', BootScene, false);
-    this.scene.add('Preload', PreloadScene, false);
-    this.scene.add('GameTitle', GameTitleScene, false);
-    this.scene.add('Main', MainScene, false);
-    this.scene.add('GameOver', GameOverScene, false);
+        super(config);
 
-    this.scene.start('Boot');
+        this.scene.add('Boot', BootScene, false);
+        this.scene.add('Preload', PreloadScene, false);
+        this.scene.add('GameTitle', GameTitleScene, false);
+        this.scene.add('Main', MainScene, false);
+        this.scene.add('GameOver', GameOverScene, false);
 
-    StatusBar.hide()
-      .catch(console.log);
-    SplashScreen.hide();
+        this.scene.start('Boot');
 
-  }
+        StatusBar.hide()
+            .catch(console.log);
+        SplashScreen.hide();
+
+    }
 
 }
 
